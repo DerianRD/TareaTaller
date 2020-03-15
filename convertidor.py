4e27f8c5dd01fb5aaddc332e8087179f9087a746
Derian <dmrodriguez2000@gmail.com>
2020-03-15 15:21:26 -0600

    Modifique para que convierta de base decimal a alguna de base menor

--- convertidor.py
+++ convertidor.py
@@ -1,11 +1,22 @@
 res=0 #resultado
-exp=0 # esponente de cada digito
-base=int(input("digite en qué base está el número a convertir")) # la base en que está el número que se desea convertir
-numero= int(input("digite numero que desea convertir"))# el número que se va a convetir
+exp=0 # exponente de cada digito
+numero= int(input("Digite numero que desea convertir "))# el número que se va a convertir
+base=int(input("Digite en qué base está el número a convertir ")) # la base en que está el número que se desea convertir
+baseDestino=int(input("Digite la base a la que desea convertir ")) #la base a la quiere convertir el numero
+conversion = []
+
 while numero>0:
     residuo=numero%10
     res=residuo*base**exp+res
     exp+=1
     numero=numero//10
-print("su resultado es",res)    
+if baseDestino != 10:
+    while res != 0:
+        conversion.append(res%baseDestino)
+        res = res // baseDestino
+    for i in reversed(conversion):
+        print(i, end="")
+else:
+    print("Su resultado es:",res)
+   
 
