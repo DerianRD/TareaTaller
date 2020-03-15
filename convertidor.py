dff07cc567788e969e4d6b97bf13f56a012fdcfc
Bryamslm <bryamlopez1882002@estudiantec.cr>
2020-03-15 14:26:49 -0600

    avance de bryam

--- convertidor.py
+++ convertidor.py
@@ -1,6 +1,11 @@
-print("Digite el numero que desea convertir")
-numero = input()
-print("Digite la base donde se encuentra el numero")
-baseOrigen = int(input())
-print("Digite la base a la que desea convertir")
-baseDestino = int(input())
+res=0 #resultado
+exp=0 # esponente de cada digito
+base=int(input("digite en qué base está el número a convertir")) # la base en que está el número que se desea convertir
+numero= int(input("digite numero que desea convertir"))# el número que se va a convetir
+while numero>0:
+    residuo=numero%10
+    res=residuo*base**exp+res
+    exp+=1
+    numero=numero//10
+print("su resultado es",res)    
+
